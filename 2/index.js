bfa2267566b3b2a8fc2e8c1eb870244f84391991
Ilya Vorobyev <bjnhks@gmail.com>
2023-12-25 19:16:02 +0300

    Add day 2 part 2

--- 2/index.js
+++ 2/index.js
@@ -1,4 +1,3 @@
-// const input = require('./example');
 const input = require('./input');
 
 // Day two part one
@@ -24,3 +23,31 @@ const answer = input.reduce((res, line) => {
 }, 0);
 
 console.log(answer);
+
+// Day two part two
+const initialCubes = {
+  red: 0,
+  green: 0,
+  blue: 0,
+};
+
+const answer2 = input.reduce((res, line) => {
+  const [game] = line.match(/(?<=Game )\d+/);
+
+  requiredCubes = Object.keys(initialCubes).reduce(
+    (acc, color) => {
+      const pattern = new RegExp(`\\d+(?= ${color})`, 'g');
+      const matchedArr = [...line.matchAll(pattern)];
+      const max = matchedArr.reduce((max, val) => Math.max(max, val[0]), 0);
+      acc[color] = max;
+      return acc;
+    },
+    { ...initialCubes }
+  );
+
+  gameValue = Object.values(requiredCubes).reduce((prod, mult) => prod * mult);
+
+  return res + gameValue;
+}, 0);
+
+console.log(answer2);
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My take on [Advent of Code](https://adventofcode.com/) using JS
 
 Just started this awesome adventure so I'm kinda late to the party.
 
-![6%](https://progress-bar.dev/6) 3/50 stars
+![8%](https://progress-bar.dev/8) 4/50 stars
 
 ## Run
 
