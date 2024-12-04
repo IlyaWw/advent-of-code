80eb3e147daa589438ff3eb20ad28f2215ea8492
Ilya Vorobyev <bjnhks@gmail.com>
2024-12-04 23:08:01 +0300

    Add 2024 day 3 part 2

--- 2024/3/index.js
+++ 2024/3/index.js
@@ -4,12 +4,32 @@ const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
 
 /********** Day three part one **********/
 
-const muls = input.matchAll(/mul\(\d+,\d+\)/g);
+const calculateMuls = (str) => {
+  const muls = str.matchAll(/mul\(\d+,\d+\)/g);
 
-const answer = Array.from(muls).reduce((res, [mul]) => {
-  const product = Array.from(mul.matchAll(/\d+/g)).reduce(([a], [b]) => a * b);
+  return Array.from(muls).reduce((res, [mul]) => {
+    const product = Array.from(mul.matchAll(/\d+/g)).reduce(
+      ([a], [b]) => a * b
+    );
 
-  return res + product;
-}, 0);
+    return res + product;
+  }, 0);
+};
+
+const answer = calculateMuls(input);
 
 console.log(answer);
+
+/********** Day three part two **********/
+
+const disabledOps = input.split(`don't()`);
+const enabledOps = [
+  disabledOps[0],
+  ...disabledOps
+    .slice(1)
+    .flatMap((disabledPart) => disabledPart.split('do()').slice(1)),
+];
+
+const answer2 = calculateMuls(enabledOps.join(''));
+
+console.log(answer2);
