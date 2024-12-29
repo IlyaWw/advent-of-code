e106eea2418afd6e550932606b486d5f2fefbb31
Ilya Vorobyev <bjnhks@gmail.com>
2024-12-29 10:32:37 +0300

    Add 2024 day 10 part 2

--- 2024/10/index.js
+++ 2024/10/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
 
+/********** Day ten part one **********/
+
 const board = input
   .split('\n')
   .map((row) => row.split('').map((cell) => Number(cell)));
@@ -47,3 +49,40 @@ const answer = board.reduce(
 );
 
 console.log(answer);
+
+/********** Day ten part two **********/
+
+const countTrails = (x, y) => {
+  let res = 0;
+
+  ortho.forEach((neighbor) => {
+    const [dx, dy] = neighbor;
+    const nx = x + dx;
+    const ny = y + dy;
+    if (board?.[ny]?.[nx]) {
+      if (board[ny][nx] - 1 === board[y][x]) {
+        if (board[ny][nx] === 9) {
+          res++;
+        } else {
+          res += countTrails(nx, ny);
+        }
+      }
+    }
+  });
+
+  return res;
+};
+
+const answer2 = board.reduce(
+  (res, row, i) =>
+    res +
+    row.reduce((res, point, j) => {
+      if (point === 0) {
+        return res + countTrails(j, i);
+      }
+      return res;
+    }, 0),
+  0
+);
+
+console.log(answer2);
